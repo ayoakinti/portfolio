e011f642deea77888fb396e0048e199764c9fd8b
Ayomide Akintimehin <ayoakinti@gmail.com>
2021-03-01 05:57:33 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `ayoakinti`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Portfolio of amazing software engineer, Ayomide Akintimehin Olawale.`,
+    author: `@iamayoakinti`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
