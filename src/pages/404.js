79613823dcff0dcfd3bdf87d70b4d3a532d08f35
Ayomide Akintimehin <ayoakinti@gmail.com>
2021-03-02 10:48:11 +0100

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,12 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <div>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="main-content">
+      <div className="container">
+        <h1>404: Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>
+    </div>
   </div>
 )
 
